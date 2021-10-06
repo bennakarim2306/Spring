7023ee95b8a1d5a6bf053704ec759ae1350fcfb6
bennakarim2306 <bennakarim@hotmail.de>
2021-10-06 14:38:12 +0200

    deleted unnecessary files

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -3,17 +3,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
-        Company company = context.getBean("company", Company.class);
-        Ceo david = context.getBean("ceo", Ceo.class);
-        System.out.println(company.toString());
     }
 }
